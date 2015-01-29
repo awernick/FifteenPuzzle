432d686a1e181352e4062285cb1178eef32c56cb
michael <michael@michael-K53E>
2015-01-29 15:43:55 -0700

    1/29 GUI Update Test

--- src/GameArea.java
+++ src/GameArea.java
@@ -1,16 +1,19 @@
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
+import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 
 /**
  * Created by awernick on 1/27/15.
  */
-public class GameArea extends JApplet
+public class GameArea extends JApplet implements MouseListener
 {
 	//Game Components Instantiation
 	private static BoardController boardController;
@@ -18,36 +21,33 @@ public class GameArea extends JApplet
 	// Define Tile Measurements
     public static final int TILE_WIDTH = 50;
     public static final int TILE_HEIGHT = 50;
-    private static int WINDOW_WIDTH = 300;
-    private static int WINDOW_HEIGHT = 500;
     
     //Instantiate GUI Components and Layout Managers
     private JPanel controlPanel;
     private JButton newGameButton;
     private JButton solveButton;
-    private BorderLayout gLayout;
-    private static JPanel gameBoard;
-    
+    private GridBagLayout gLayout;
+    private static JPanel gameBoard;    
     
     public GameArea()
     {
-    	BoardController boardController = new BoardController();
-    	boardController.scrambleBoard();
-    	this.initGui();    	
+    	boardController = new BoardController();
+    	
+    	initGui();
     }
     
-    /**
-     * No purpose as of 1/28/2015
-     * */
-    public void init(){}
-    
+    public void paint()
+    {
+    	initGui();
+    }
     /**
      * Initializes main Pane and adds the control pane together with the board
      * */
     private void initGui()
     {
     	//Initialize Components
-		gLayout = new BorderLayout();
+		gLayout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
     	controlPanel = new JPanel();
     	newGameButton = new JButton("New Game");
     	solveButton = new JButton("Solve");
@@ -60,10 +60,38 @@ public class GameArea extends JApplet
     	
     	//Add Panel to main component
     	this.setLayout(gLayout);
-    	this.add(controlPanel,BorderLayout.SOUTH);
-    	this.add(new JSeparator(), BorderLayout.CENTER);
-    	this.add(refreshTiles(boardController.getBoard()), BorderLayout.BEFORE_LINE_BEGINS);
+    	c.fill = GridBagConstraints.HORIZONTAL;
+    	c.gridx = 0;
+    	c.gridy = 0;
+    	c.insets = new Insets(10,0,0,0);
+    	this.add(controlPanel,c);
+    	
+    	//Set up Layout Manager for separator
+    	c.insets = new Insets(0,0,0,0);
+    	c.gridy = 1;
+    	this.add(new JSeparator(), c);
+    	
+    	c.fill = GridBagConstraints.VERTICAL;
+    	c.gridx = 0;
+    	c.gridy = 2;
+    	this.add(new JLabel("Moves = " + boardController.getUserMoves()), c);
+    	
+    	c.fill = GridBagConstraints.VERTICAL;
+
+    	c.ipady = 400;
+    	c.ipadx = 400;
+    	c.weightx = 1;
+    	c.weighty = 1;
+    	c.gridwidth = 1;
+    	c.gridx = 0;
+    	c.gridy = 3;
+
+    	c.insets = new Insets(5, 5, 5, 5);
+    	gameBoard = refreshTiles(boardController.getBoard());
+    	this.add(gameBoard,c);
+    	
     	this.setPreferredSize(getPreferredSize());
+
     }
     
     
@@ -83,6 +111,7 @@ public class GameArea extends JApplet
 		for(int i = 0; i < 4; i++)
 			for(int j = 0; j < 4; j++)
 			{
+				//Empty Button
 				if(tiles[i][j].getValue()<0)
 				{
 					currentButton = new JButton();
@@ -90,6 +119,7 @@ public class GameArea extends JApplet
 					currentButton.setBackground(Color.white);
 					gameBoard.add(currentButton);
 				}else{
+				//Numbered Button
 				currentButton = new JButton(Integer.toString(tiles[i][j].getValue()));
 				currentButton.setPreferredSize(new Dimension(TILE_WIDTH,TILE_HEIGHT));;
 				currentButton.setBackground(Color.gray);
@@ -97,5 +127,35 @@ public class GameArea extends JApplet
 				}
 			}
 		return gameBoard;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
